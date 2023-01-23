dc22cda69b24e36e6966fa134a7baf6c778897ab
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-01-23 04:39:06 -0300

    feat: created habit page with components as title and progress bar and some checkbox

--- src/screens/Habit.tsx
+++ src/screens/Habit.tsx
@@ -1,10 +1,35 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import dayjs from "dayjs";
+import { useRoute } from "@react-navigation/native";
+import { BackButton } from "../components/BackButton";
+import { ProgressBar } from "../components/ProgressBar";
+import { CheckBox } from "../components/Checkbox";
 
+interface Params {
+  date: string;
+}
 export const Habit = () => {
+  const route = useRoute();
+  const { date } = route.params as Params;
+  const parsedDate = dayjs(date);
+
+  const dayOfWeek = parsedDate.format("dddd");
+  const dayAndMonth = parsedDate.format("DD/MM");
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <Text>Habit</Text>
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 30 }}>
+        <BackButton />
+        <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">{dayOfWeek}</Text>
+        <Text className="text-white font-extrabold text-3xl">{dayAndMonth}</Text>
+
+        <ProgressBar progress={60} />
+
+        <View className="mt-6">
+          <CheckBox title="Beber 2L de agua" />
+          <CheckBox title="Caminhar" checked />
+        </View>
+      </ScrollView>
     </View>
   );
 };
