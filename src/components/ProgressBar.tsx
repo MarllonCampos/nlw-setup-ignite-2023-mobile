fd7985abb4198fa38656a480da9fc96a6d8a5d98
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-01-25 04:28:03 -0300

    feat: using animated view and shared value and with timing to create a animation when progress bar changes

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View } from "react-native";
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 
 // import { Container } from './styles';
 interface Props {
   progress?: number;
 }
 export const ProgressBar = ({ progress = 0 }: Props) => {
-  const parsedProgress = progress > 100 ? 100 : progress;
+  const sharedProgress = useSharedValue(progress);
+  const style = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`,
+    };
+  });
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(progress);
+  }, [progress]);
   return (
     <View className="2-full h-3 rounded-xl bg-zinc-700 mt-4">
-      <View className="h-3 rounded-xl bg-violet-600" style={{ width: `${progress}%` }} />
+      <Animated.View className="h-3 rounded-xl bg-violet-600" style={style} />
     </View>
   );
 };
