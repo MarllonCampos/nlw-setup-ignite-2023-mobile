a7c541cdc937f8435fdd899f7ac1b35f10447058
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-01-25 04:32:47 -0300

    feat: adding animations to animated view entering

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { View, TouchableOpacity, TouchableOpacityProps, Text } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
-
+import Animated, { ZoomInRotate } from "react-native-reanimated";
 interface Props extends TouchableOpacityProps {
   title: string;
   checked?: boolean;
@@ -12,9 +12,9 @@ export const CheckBox = ({ title, checked = false, ...rest }: Props) => {
   return (
     <TouchableOpacity activeOpacity={0.7} className="flex-row mb-2 items-center" {...rest}>
       {checked ? (
-        <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
+        <Animated.View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center" entering={ZoomInRotate}>
           <Feather name="check" size={20} color={colors.white} />
-        </View>
+        </Animated.View>
       ) : (
         <View className="h-8 w-8 bg-zinc-800 rounded-lg" />
       )}
